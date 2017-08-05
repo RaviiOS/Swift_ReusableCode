41b7ee0fe6c2c8c702f02a10abdf42d203d02b36
Ravi <yaganti.ravikumar@gmail.com>
2017-08-05 23:22:44 +0530

    > Updated global activity indicator for UIViewController

--- RKSwift3Plus/RKSwift3Plus/Extensions/UIViewController.swift
+++ RKSwift3Plus/RKSwift3Plus/Extensions/UIViewController.swift
@@ -2,7 +2,56 @@
 import Foundation
 import UIKit
 public extension UIViewController {
-
+    var activityIndicatorTag: Int { return 999999 }
+    
+    func startActivityIndicator(
+        style: UIActivityIndicatorViewStyle = .gray,
+        location: CGPoint? = nil) {
+        
+        //Set the position - defaults to `center` if no`location`
+        
+        //argument is provided
+        
+        let loc = location ?? self.view.center
+        
+        //Ensure the UI is updated from the main thread
+        
+        //in case this method is called from a closure
+        
+        DispatchQueue.main.async(execute: {
+            
+            //Create the activity indicator
+            
+            let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: style)
+            //Add the tag so we can find the view in order to remove it later
+            
+            activityIndicator.tag = self.activityIndicatorTag
+            //Set the location
+            
+            activityIndicator.center = loc
+            activityIndicator.hidesWhenStopped = true
+            //Start animating and add the view
+            
+            activityIndicator.startAnimating()
+            self.view.addSubview(activityIndicator)
+        })
+    }
+    
+    func stopActivityIndicator() {
+        
+        //Again, we need to ensure the UI is updated from the main thread!
+        
+        DispatchQueue.main.async(execute: {
+            //Here we find the `UIActivityIndicatorView` and remove it from the view
+            
+            if let activityIndicator = self.view.subviews.filter(
+                { $0.tag == self.activityIndicatorTag}).first as? UIActivityIndicatorView {
+                activityIndicator.stopAnimating()
+                activityIndicator.removeFromSuperview()
+            }
+        })
+    }
+    
     /// shows an UIAlertController alert with error title and message
     public func showError(_ title: String, message: String? = nil) {
         if !Thread.current.isMainThread {
